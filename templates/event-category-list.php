8d07fc9745089dfeaf7d2319120825355625ba24
Suraj Gupta <suraj.gupta.489@gmail.com>
2017-08-01 20:52:48 +0530

    Provide Shortcode Parameters

--- templates/event-category-list.php
+++ templates/event-category-list.php
@@ -1,16 +1,18 @@
 <?php
-$categories = $this->get('categories');
-$disable = $this->get('disable');
+    $categories = $this->get('categories');
+    $disable    = $this->get('disable');
+    $page_link  = $this->get('page_link');
 
-if (!isset($disable['animation']) || !$disable['animation']) {
-    ?>
-    <style type="text/css">
-        .lo-f1-container:hover .lo-f1-card {
-            transform: none !important;
-        }
-    </style>
-    <?php
-}
+    if ( ! isset($disable['animation']) || ! $disable['animation'])
+    {
+        ?>
+        <style type="text/css">
+            .lo-f1-container:hover .lo-f1-card {
+                transform: none !important;
+            }
+        </style>
+        <?php
+    }
 ?>
 <!-- Page Content -->
 <div class="container">
@@ -20,38 +22,47 @@ if (!isset($disable['animation']) || !$disable['animation']) {
     <div class="row">
 
         <?php
-        if (!empty($categories)) {
-            foreach ($categories as $index => $category) {
-                ?>
+            if ( ! empty($categories))
+            {
+                foreach ($categories as $index => $category)
+                {
+                    $link = $category->term_link;
+                    if ( ! empty($page_link['page_query_arr']))
+                    {
+                        $link = $category->term_link . '?' . http_build_query($page_link['page_query_arr']);
+                    }
+                    ?>
 
-                <div class="col-md-4 col-sm-12">
-                    <div class="lo-f1-container" onclick="javascript:window.location = '<?php echo $category->term_link ?>'">
-                        <div class="lo-f1-card">
-                            <div class="lo-front lo-face">
-                                <?php
-                                if (!empty($category->image_url)) {
-                                    echo '<img src="' . $category->image_url . '" alt="Category Image" width="125" />';
-                                }
-                                ?>
-                                <span class="lo-project-heading"><?php echo $category->name ?></span>
-                            </div>
-                            <div class="lo-back lo-face center">
-                                <p><span class="lo-project-heading"><?php echo $category->name ?></span></p>
-                                <?php
-                                if (!empty($category->image_url)) {
-                                    echo '<img src="' . $category->image_url . '" alt="Category Image" width="35" style="margin-bottom:5px;" />';
-                                }
-                                ?>
-                                <p><?php echo substr($category->description, 0, 150) . '...' ?></p>
-                                <a href="<?php echo $category->term_link ?>" class="lo-view-proj-num">View Projects</a>
+                    <div class="col-md-4 col-sm-12">
+                        <div class="lo-f1-container" onclick="javascript:window.location = '<?php echo $link ?>'">
+                            <div class="lo-f1-card">
+                                <div class="lo-front lo-face">
+                                    <?php
+                                        if ( ! empty($category->image_url))
+                                        {
+                                            echo '<img src="' . $category->image_url . '" alt="Category Image" width="125" />';
+                                        }
+                                    ?>
+                                    <span class="lo-project-heading"><?php echo $category->name ?></span>
+                                </div>
+                                <div class="lo-back lo-face center">
+                                    <p><span class="lo-project-heading"><?php echo $category->name ?></span></p>
+                                    <?php
+                                        if ( ! empty($category->image_url))
+                                        {
+                                            echo '<img src="' . $category->image_url . '" alt="Category Image" width="35" style="margin-bottom:5px;" />';
+                                        }
+                                    ?>
+                                    <p><?php echo substr($category->description, 0, 150) . '...' ?></p>
+                                    <a href="<?php echo $link ?>" class="lo-view-proj-num">View Projects</a>
+                                </div>
                             </div>
                         </div>
                     </div>
-                </div>
 
-                <?php
+                    <?php
+                }
             }
-        }
         ?>
 
     </div>
--- templates/search-form.php
+++ templates/search-form.php
@@ -1,12 +1,21 @@
+<?php
+    $page_link = $this->get('page_link');
+?>
 <div class="panel panel-default">
     <div class="panel-body">
         <?php
-        $lo_events_page_settings = lo_get_option('page', 'all');
-        $projects_srch_link = !empty($lo_events_page_settings['lo_events_page_lo_search_page'])
-            ? get_permalink($lo_events_page_settings['lo_events_page_lo_search_page'])
-            : get_permalink(get_page_by_path('search-projects'));
+            $lo_events_page_settings = lo_get_option('page', 'all');
+            $projects_srch_link      = ! empty($lo_events_page_settings['lo_events_page_lo_search_page'])
+                ? get_permalink($lo_events_page_settings['lo_events_page_lo_search_page'])
+                : get_permalink(get_page_by_path('search-projects'));
         ?>
         <form action="<?php echo $projects_srch_link ?>" id="lo-event-search-form" method="GET">
+            <?php
+                if ( ! empty($page_link['page_query_arr']['lo-cat-page']))
+                {
+                    echo '<input type="hidden" name="lo-cat-page" value="' . $page_link['page_query_arr']['lo-cat-page'] . '" />';
+                }
+            ?>
             <div class="form-horizontal">
                 <div class="form-group col-sm-12">
                     <label class="lo-filter-col col-sm-2 control-label lo-text-align-left lo-event-search-label"
@@ -28,27 +37,36 @@
 
             <div class="form-horizontal">
                 <div id="lo-event-form-advanced-option" class="lo-event-form-advanced-option">
-                    <div class="form-group col-sm-12">
-                        <label class="lo-filter-col col-sm-2 control-label lo-text-align-left"
-                               style="margin-right:0;" for="lo-event-cat">Project
-                            Types</label>
-                        <div class="col-sm-8">
-                            <select id="lo-event-cat" name="lo-event-cat"
-                                    class="form-control">
-                                <option value="">All Project Types</option>
-								<?php
-									if ( ! empty( $this->get( 'categories' ) ) ) {
-										foreach (
-											$this->get( 'categories' ) as $index => $category
-										) {
-											echo '<option value="' . $category->term_id . '">' .
-											     $category->name . '</option>';
-										}
-									}
-								?>
-                            </select>
-                        </div>
-                    </div> <!-- form group [order by] -->
+                    <?php
+                        if (empty($page_link['page_query_arr']['lo-cat-page']))
+                        {
+                            ?>
+                            <div class="form-group col-sm-12">
+                                <label class="lo-filter-col col-sm-2 control-label lo-text-align-left"
+                                       style="margin-right:0;" for="lo-event-cat">Project Types
+                                </label>
+                                <div class="col-sm-8">
+                                    <select id="lo-event-cat" name="lo-event-cat"
+                                            class="form-control">
+                                        <option value="">All Project Types</option>
+                                        <?php
+                                            if ( ! empty($this->get('categories')))
+                                            {
+                                                foreach (
+                                                    $this->get('categories') as $index => $category
+                                                )
+                                                {
+                                                    echo '<option value="' . $category->term_id . '">' .
+                                                         $category->name . '</option>';
+                                                }
+                                            }
+                                        ?>
+                                    </select>
+                                </div>
+                            </div> <!-- form group [order by] -->
+                            <?php
+                        }
+                    ?>
                     <div class="form-group col-sm-12">
                         <label class="lo-filter-col col-sm-2 control-label lo-text-align-left"
                                style="margin-right:0;"
@@ -57,26 +75,30 @@
                             <select id="lo-event-org" name="lo-event-org"
                                     class="form-control">
                                 <option value="">All Project Partners</option>
-								<?php
-									if ( ! empty( $this->get( 'partners' ) ) ) {
-										foreach (
-											$this->get( 'partners' ) as $index => $partner
-										) {
-											if ( $partner instanceof WP_Post ) {
-												
-												echo '<option value="' . get_post_meta($partner->ID, 'lo_ccb_event_partner_group_id', 1) .
-												     '">' .
-												     $partner->post_title . '</option>';
-											} else {
-												
-												echo '<option value="' . $partner->get_meta('lo_ccb_event_partner_group_id') .
-												     '">' .
-												     $partner->title() . '</option>';
-											}
-											
-										}
-									}
-								?>
+                                <?php
+                                    if ( ! empty($this->get('partners')))
+                                    {
+                                        foreach (
+                                            $this->get('partners') as $index => $partner
+                                        )
+                                        {
+                                            if ($partner instanceof WP_Post)
+                                            {
+
+                                                echo '<option value="' . get_post_meta($partner->ID, 'lo_ccb_event_partner_group_id', 1) .
+                                                     '">' .
+                                                     $partner->post_title . '</option>';
+                                            } else
+                                            {
+
+                                                echo '<option value="' . $partner->get_meta('lo_ccb_event_partner_group_id') .
+                                                     '">' .
+                                                     $partner->title() . '</option>';
+                                            }
+
+                                        }
+                                    }
+                                ?>
                             </select>
                         </div>
                     </div> <!-- form group [order by] -->
@@ -105,15 +127,17 @@
                             <select id="lo-event-loc" name="lo-event-loc"
                                     class="form-control">
                                 <option value="">All Locations</option>
-								<?php
-									if ( ! empty( $this->get( 'cities' ) ) ) {
-										echo '<ul class="dropdown-menu lo-dropdown-menu">';
-										foreach ( $this->get( 'cities' ) as $val ) {
-											echo '<option>' . $val . '</option>';
-										}
-										echo '</ul>';
-									}
-								?>
+                                <?php
+                                    if ( ! empty($this->get('cities')))
+                                    {
+                                        echo '<ul class="dropdown-menu lo-dropdown-menu">';
+                                        foreach ($this->get('cities') as $val)
+                                        {
+                                            echo '<option>' . $val . '</option>';
+                                        }
+                                        echo '</ul>';
+                                    }
+                                ?>
                             </select>
                         </div>
                     </div> <!-- form group [order by] -->
