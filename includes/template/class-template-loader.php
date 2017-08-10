0d35000b6fde8712adf8d37c920a930f1ffed6b1
Dave Mackey <davem@liquidchurch.com>
2017-08-10 10:19:38 -0400

    - Updated phpdoc in class-template-loader.php, was indicating a return of void but actually is returning the string $located.

--- includes/template/class-template-loader.php
+++ includes/template/class-template-loader.php
@@ -147,7 +147,7 @@
 		 *
 		 * @param  string $template Template file to search for.
 		 *
-		 * @return void
+		 * @return string $located
 		 */
 		protected function _locate( $template ) {
 			$theme_file_path = '/liquid-outreach/' . $template;
